a6ad741ea008f77ae1ad0723b19f90cb9c330057
HANXU <1076998404@qq.com>
2020-04-01 09:09:12 +0800

    Test conflict resolution
    测试出现冲突解决

--- 第一阶段PAT基础编程题目集/[DAY0_2020-3-31]7-1厘米换算英尺英寸 (15分)/JAVA/HANXU2018/Main.java
+++ 第一阶段PAT基础编程题目集/[DAY0_2020-3-31]7-1厘米换算英尺英寸 (15分)/JAVA/HANXU2018/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("测试出现冲突解决");
         Scanner in = new Scanner(System.in);
         System.out.println("尝试改变代码");
         while (in.hasNextInt()) {
